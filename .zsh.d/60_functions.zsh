dd63ca51c087b30c9841f10a3d407457bf31e3c4
Mauro Tortonesi <mauro.tortonesi@unife.it>
2012-03-29 15:37:59 +0200

    Fixed genpasswd function.

--- .zsh.d/60_functions.zsh
+++ .zsh.d/60_functions.zsh
@@ -4,10 +4,13 @@ function mkcd {
     cd "$*"
 }
 
-# Random password generation
+# Random password generation function
 function genpasswd {
-  local l=$1
-  [ "$l" == "" ] && l=16
+  local l=12 # default password lenght
+  if [ "$#" != "0" -a "$1" -gt 0 ]
+  then
+    l=$1
+  fi
   tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
 }
 
