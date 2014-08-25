b7d9adfe48fb7a52d5f1e2a10bd8b46108fe51d2
Mauro Tortonesi <mauro.tortonesi@unife.it>
2014-08-25 15:12:10 +0200

    Forgot to remove debug echo statement.

--- .zsh.d/48_mc.zsh
+++ .zsh.d/48_mc.zsh
@@ -4,7 +4,6 @@ if [ -f /usr/lib/mc/mc-wrapper.sh ]; then
 elif [ -f /usr/local/bin/brew -a -f /usr/local/bin/jq ]; then
     mc_version=`brew info --json=v1 mc | jq -r '.[].installed | max_by(.version) .version'`
     if [ -f "/usr/local/Cellar/midnight-commander/${mc_version}/libexec/mc/mc-wrapper.sh" ]; then
-      echo "found /usr/local/Cellar/midnight-commander/${mc_version}/libexec/mc/mc-wrapper.sh"
       alias mc=". /usr/local/Cellar/midnight-commander/${mc_version}/libexec/mc/mc-wrapper.sh"
     fi
 fi
