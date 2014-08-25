c112cd8970768c2a9cbc50323005582d7dea3eec
Mauro Tortonesi <mauro.tortonesi@unife.it>
2014-08-25 14:18:58 +0200

    Got mc_wrapper to work on OS X as well.

--- .zsh.d/48_mc.zsh
+++ .zsh.d/48_mc.zsh
@@ -1,11 +1,14 @@
 # alias for midnight commander
-if [ -f /usr/lib/mc/mc-wrapper.sh ]
-then
+if [ -f /usr/lib/mc/mc-wrapper.sh ]; then
     alias mc='. /usr/lib/mc/mc-wrapper.sh'
+elif [ -f /usr/local/bin/brew -a -f /usr/local/bin/jq ]; then
+    mc_version=`brew info --json=v1 mc | jq -r '.[].installed | max_by(.version) .version'`
+    if [ -f "/usr/local/Cellar/midnight-commander/${mc_version}/libexec/mc/mc-wrapper.sh" ]; then
+      echo "found /usr/local/Cellar/midnight-commander/${mc_version}/libexec/mc/mc-wrapper.sh"
+      alias mc=". /usr/local/Cellar/midnight-commander/${mc_version}/libexec/mc/mc-wrapper.sh"
+    fi
 fi
 
-if [ -f $HOME/.local/share/mc/skins/solarized.ini ]
-then
+if [ -f $HOME/.local/share/mc/skins/solarized.ini ]; then
     export MC_SKIN=$HOME/.local/share/mc/skins/solarized.ini
 fi
-
