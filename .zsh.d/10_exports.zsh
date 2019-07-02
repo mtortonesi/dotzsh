a533e61d3edb32e64e216af1040039cb9c6f3d37
Mauro Tortonesi <mauro.tortonesi@unife.it>
2019-07-02 22:43:07 +0200

    Switch from vim to neovim.

--- .zsh.d/10_exports.zsh
+++ .zsh.d/10_exports.zsh
@@ -15,9 +15,9 @@ export UNAME="$(uname)"
 
 # Some applications read the EDITOR variable to determine your favourite text
 # editor. So uncomment the line below and enter the editor of your choice :-)
-export EDITOR='vim'
-export GIT_EDITOR='vim'
-export VISUAL='vim'
+export EDITOR='nvim'
+export GIT_EDITOR='nvim'
+export VISUAL='nvim'
 
 # Search in ~/bin and ~/opt/node.js/bin first, then in PATH
 if [ -d ~/opt/node.js ]
