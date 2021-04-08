d08d510d215529c1ba30af7211aa316e01fdcb85
Mauro Tortonesi <mauro.tortonesi@unife.it>
2021-04-08 12:13:24 +0200

    Fix mc misbehavior with zsh fancy prompts.

--- .zsh.d/48_mc.zsh
+++ .zsh.d/48_mc.zsh
@@ -1,19 +1,16 @@
-# alias for midnight commander
+# Setup alias for midnight commander
+# 1) use mc-wrapper script if available (as it allows mc to remember the last
+#    directory it was used from)
+# 2) use -u option to tell mc not to launch a concurrent shell (doing it
+#    wouldn't play well with powerlevel10k and more in general with zsh fancy
+#    prompts and could lead to unpleasant misbehaviors including hang on startup)
 if [ -f /usr/lib/mc/mc-wrapper.sh ]; then
-    alias mc='. /usr/lib/mc/mc-wrapper.sh'
+    alias mc='. /usr/lib/mc/mc-wrapper.sh -u'
 elif [ -f /usr/local/bin/brew -a -f /usr/local/bin/jq ]; then
     mc_version=`brew info --json=v1 mc | jq -r '.[].installed | max_by(.version) .version'`
     if [ -f "/usr/local/Cellar/midnight-commander/${mc_version}/libexec/mc/mc-wrapper.sh" ]; then
-      alias mc=". /usr/local/Cellar/midnight-commander/${mc_version}/libexec/mc/mc-wrapper.sh"
+      alias mc=". /usr/local/Cellar/midnight-commander/${mc_version}/libexec/mc/mc-wrapper.sh -u"
     fi
+else
+    alias mc='mc -u'
 fi
-
-if [ -n "$SOLARIZED" -a -f $HOME/.local/share/mc/skins/solarized.ini ]; then
-    export MC_SKIN=$HOME/.local/share/mc/skins/solarized.ini
-fi
-
-## disable right prompt inside midnight commander
-## (taken from http://www.zsh.org/mla/users/2013/msg00402.html)
-#if [[ $+MC_SID = 1 ]] ; then
-#    RPROMPT=""
-#fi
