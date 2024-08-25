13e6149804e296989ea49387c3259d03e33fd2ac
Mauro Tortonesi <mauro.tortonesi@unife.it>
2024-08-25 10:33:30 +0200

    Minor bugfixing and removal of unused configs.

--- .zsh.d/20_modules_disabled.zsh
+++
@@ -1,9 +0,0 @@
-# .zsh.d/20_modules.zsh
-
-# Enable zargs, a superior alternative to xargs
-autoload -U zargs
-
-# Enable the super powerful zmv file renaming builtin tool
-# autoload -U zmv
-# alias zcp='zmv -C'
-# alias zln='zmv -L'
